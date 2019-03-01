73ceccd59ab32fac2d3ed0c58198e57470cf42be
WeitaoMiao <31043477+WeitaoMiao@users.noreply.github.com>
2019-03-01 19:13:24 +0800

    Update TrapezoidalFormula.h

--- CMath/Analyse/NumCalculus/Integral/TrapIntegral/TrapezoidalFormula.h
+++ CMath/Analyse/NumCalculus/Integral/TrapIntegral/TrapezoidalFormula.h
@@ -1,6 +1,6 @@
 #ifndef _TRAPEZOIDALFORMULA_H_
 #define _TRAPEZOIDALFORMULA_H_
 
-double TrapezoidalFormula(float Ceiling, float Lower, float H, int N)
+double TrapezoidalFormula(float Ceiling, float Lower, float H, int N);
 
 #endif
