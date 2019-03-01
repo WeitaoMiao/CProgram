2dc036c3cf3697433b45563eb83d9ad7b380ee0a
WeitaoMiao <31043477+WeitaoMiao@users.noreply.github.com>
2019-03-01 19:11:52 +0800

    Update main.c

--- CMath/Analyse/NumCalculus/Integral/TrapIntegral/main.c
+++ CMath/Analyse/NumCalculus/Integral/TrapIntegral/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+#include "TrapezoidalFormula.h"
 
 //(复合形式)梯形公式
 double TrapezoidalFormula(float Ceiling, float Lower, float H, int N);
@@ -43,40 +44,6 @@ int main(void)
     printf("\n");
     return 0;
 }
-
-
-//梯形法计算公式
-double TrapezoidalFormula(float Ceiling, float Lower, float H, int N)
-{
-    double Result;
-    double PartFirst, PartSecond;
-    double Sum;
-    float xk;
-    int k;
-    
-    //初始化
-    Result = 0;
-    PartFirst = 0;
-    PartSecond = 0;
-    xk = 0;
-    Sum = 0;
-    k = 0;
-    
-    //第一部分
-    PartFirst = H*0.5*(IntegrandFcn(Ceiling)+IntegrandFcn(Lower));
-    //第二部分
-    for(k=1; k<=N-1; k++)
-    {
-        xk = Lower+k*H;
-        Sum = Sum+IntegrandFcn(xk);
-    }
-    PartSecond = H*Sum;
-    
-    Result = PartFirst+PartSecond;
-    
-    return Result;
-}
-
 
 //被积函数
 double IntegrandFcn(float x)
